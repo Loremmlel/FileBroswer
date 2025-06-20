dd4d962d3665aafc011e7adb2a588d7eeed0193a
Loremmlel <loremmlel@163.com>
2025-06-20 16:12:18 +0800

    build(composeApp): 添加 AndroidX Navigation Compose 依赖
    - 在 composeApp/build.gradle.kts 中添加 AndroidX Navigation Compose 依赖- 在 gradle/libs.versions.toml 中添加 AndroidX Navigation 版本号和 Compose 依赖

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -58,6 +58,7 @@ kotlin {
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodel)
             implementation(libs.androidx.lifecycle.runtimeCompose)
+            implementation(libs.androidx.navigation.compose)
             implementation(projects.shared)
         }
         commonTest.dependencies {
