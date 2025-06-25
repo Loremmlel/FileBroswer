13fb4c9c58f955256475010a5a34fa92a7b86930
Loremmlel <loremmlel@163.com>
2025-06-25 10:45:15 +0800

    fix(server): 修复文件类型判断逻辑
    - 更新 IMAGE_SUFFIX 和 VIDEO_SUFFIX 列表，移除点号前缀
    - 修改文件类型判断逻辑，使用 extension 属性替代 endsWith 方法
    - 这个修改解决了文件名末尾包含特殊字符时可能导致的错误判断问题

--- server/src/main/kotlin/tenshi/hinanawi/filebrowser/util/FileUtil.kt
+++ server/src/main/kotlin/tenshi/hinanawi/filebrowser/util/FileUtil.kt
@@ -3,12 +3,15 @@ package tenshi.hinanawi.filebrowser.util
 import tenshi.hinanawi.filebrowser.model.FileType
 import java.io.File
 
-internal val IMAGE_SUFFIX = listOf(".png", ".jpg", ".jpeg", ".gif", ".webp", ".bmp", ".svg", ".ico")
-internal val VIDEO_SUFFIX = listOf(".mp4", ".mkv", ".avi", ".mov", ".wmv", ".flv", ".3gp")
+internal val IMAGE_SUFFIX = listOf("png", "jpg", "jpeg", "gif", "webp", "bmp", "svg", "ico")
+internal val VIDEO_SUFFIX = listOf("mp4", "mkv", "avi", "mov", "wmv", "flv", "3gp")
 
 internal fun File.getFileType(): FileType = when {
     isDirectory -> FileType.Folder
-    IMAGE_SUFFIX.any { this.endsWith(it) } -> FileType.Image
-    VIDEO_SUFFIX.any { this.endsWith(it) } -> FileType.Video
+    // bugfix
+    // 原来直接用的File.endsWith，发现如果参数是String会直接用File()构造，也就是说，endsWith实际上要传的是文件路径……
+    // 比扩展名用这个方式更合适
+    IMAGE_SUFFIX.any { this.extension.endsWith(it) } -> FileType.Image
+    VIDEO_SUFFIX.any { this.extension.endsWith(it) } -> FileType.Video
     else -> FileType.Other
 }
