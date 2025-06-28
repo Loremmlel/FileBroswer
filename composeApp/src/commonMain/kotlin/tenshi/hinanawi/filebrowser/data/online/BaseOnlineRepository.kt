8c072c3f4d26745b03e6b65c6df413305ecae0ee
柚子 <loremmlel@163.com>
2025-06-29 01:46:18 +0800

    fix(filebrowser): 优化 API响应的序列化处理
    - 引入 ResponseWithoutData 类，用于忽略未知键的 JSON 解析
    - 在 BaseOnlineRepository 中配置 Json 解析器，启用 ignoreUnknownKeys
    - 解决了泛型类和 kotlinx.serialization 序列化器匹配的问题

--- composeApp/src/commonMain/kotlin/tenshi/hinanawi/filebrowser/data/online/BaseOnlineRepository.kt
+++ composeApp/src/commonMain/kotlin/tenshi/hinanawi/filebrowser/data/online/BaseOnlineRepository.kt
@@ -5,15 +5,21 @@ import io.ktor.client.call.body
 import io.ktor.client.plugins.HttpResponseValidator
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
 import tenshi.hinanawi.filebrowser.exception.ApiException
-import tenshi.hinanawi.filebrowser.model.Response
+import tenshi.hinanawi.filebrowser.model.ResponseWithoutData
 
 abstract class BaseOnlineRepository {
     protected val client = _client
     companion object {
         private val _client = HttpClient {
             install(ContentNegotiation) {
-                json()
+                json(Json {
+                    // 呵呵，纠结我两个多小时的问题，泛型类和kotlinx.serialization序列化器匹配，就这么丑陋的解决了
+                    // 响应拦截器不关心类型，所以创建一个没有data的Response即可
+                    // 然后启用忽略未知键，就可以了，具体类型在后面请求拿到
+                    ignoreUnknownKeys = true
+                })
             }
             HttpResponseValidator {
                 validateResponse { response ->
@@ -22,7 +28,7 @@ abstract class BaseOnlineRepository {
                     if (contentType != "application/json") {
                         return@validateResponse
                     }
-                    val body = response.body<Response<*>>()
+                    val body = response.body<ResponseWithoutData>()
                     if (body.code in 400..599) {
                         throw ApiException(body.code, body.message)
                     }
--- shared/src/commonMain/kotlin/tenshi/hinanawi/filebrowser/model/Response.kt
+++ shared/src/commonMain/kotlin/tenshi/hinanawi/filebrowser/model/Response.kt
@@ -3,6 +3,12 @@ package tenshi.hinanawi.filebrowser.model
 import kotlinx.serialization.Serializable
 
 @Serializable
+data class ResponseWithoutData(
+    val code: Int,
+    val message: Message
+)
+
+@Serializable
 data class Response<T>(
     val code: Int,
     val message: Message,
