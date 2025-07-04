8552d717227d1408ff21dadfd8b89e1d573e1fcb
Loremmlel <loremmlel@163.com>
2025-07-04 11:04:03 +0800

    test: 创建测试套件整合所有测试类
    - 添加了测试套件类 ApplicationTest，整合了以下测试类：
      - FilesEndpointTest
      - RandomEndpointTest - PathValidatorTest
    - 使用 Suite 注解创建测试套件，提高测试的组织性和可维护性

--- server/src/test/kotlin/tenshi/hinanawi/filebrowser/ApplicationTest.kt
+++ server/src/test/kotlin/tenshi/hinanawi/filebrowser/ApplicationTest.kt
@@ -1,3 +1,15 @@
 package tenshi.hinanawi.filebrowser
 
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+import tenshi.hinanawi.filebrowser.plugin.PathValidatorTest
+import tenshi.hinanawi.filebrowser.route.FilesEndpointTest
+import tenshi.hinanawi.filebrowser.route.RandomEndpointTest
+
+@RunWith(Suite::class)
+@Suite.SuiteClasses(
+  FilesEndpointTest::class,
+  RandomEndpointTest::class,
+  PathValidatorTest::class
+)
 class ApplicationTest
