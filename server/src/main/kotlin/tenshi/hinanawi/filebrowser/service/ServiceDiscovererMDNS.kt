80fd08e29e27b83c0a1bc6388689e36a2c1e9bd7
Loremmlel <loremmlel@163.com>
2025-07-29 10:57:50 +0800

    feat(service): 添加 MDNS 服务发现功能
    - 在 ServiceDiscovererMDNS 类中添加了服务发现功能
    - 使用 Kotlin 协程实现异步服务搜索
    - 日志记录发现的服务信息

--- server/src/main/kotlin/tenshi/hinanawi/filebrowser/service/ServiceDiscovererMDNS.kt
+++ server/src/main/kotlin/tenshi/hinanawi/filebrowser/service/ServiceDiscovererMDNS.kt
@@ -1,10 +1,15 @@
 package tenshi.hinanawi.filebrowser.service
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import org.slf4j.LoggerFactory
 import tenshi.hinanawi.filebrowser.SERVER_PORT
 import java.net.InetAddress
 import javax.jmdns.JmDNS
 import javax.jmdns.ServiceInfo
+import kotlin.math.log
 
 class ServiceDiscovererMDNS {
   private val logger = LoggerFactory.getLogger(ServiceDiscovererMDNS::class.java)
@@ -20,6 +25,14 @@ class ServiceDiscovererMDNS {
       "柚子的文件服务器"
     )
     jmdns?.registerService(serviceInfo)
+    CoroutineScope(Dispatchers.IO).launch {
+      delay(2000)
+      logger.info("开始寻找服务")
+      val services = jmdns?.list("_http._tcp.local.")
+      services?.forEach { service ->
+        logger.info("发现服务: ${service.name} - ${service.server}:${service.port}")
+      } ?: logger.info("没有发现服务")
+    }
     logger.info("mDNS服务已经注册: ${serviceInfo.name} 在端口: $SERVER_PORT")
   } catch (e: Exception) {
     logger.error("启动mDNS服务失败", e)
